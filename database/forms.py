69617f1527dbfd7397551994b3536207f440ad07
Dylan Klintworth <dylan.klintworth@gmail.com>
2020-04-04 21:17:24 -0500

    created registration and login form

--- database/forms.py
+++ database/forms.py
@@ -6,3 +6,22 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 class SearchForm(FlaskForm):
     record_name = StringField('Record Search', validators=[DataRequired()])
     submit = SubmitField('Search')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired()])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- database/routes.py
+++ database/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request, abort
 from database import app, db
-from database.forms import SearchForm
+from database.forms import SearchForm, RegistrationForm, LoginForm
 
 
 @app.route('/')
@@ -16,3 +16,24 @@ def search():
         flash(f"Search completed for {form.record_name.data}!", 'success')
         return redirect(url_for('home'))
     return render_template('search.html', title='Search Records', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register!', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
